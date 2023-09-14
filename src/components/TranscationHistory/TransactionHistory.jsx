93097245c41f023d82b66fb91be20316acf19b7a
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-09-14 20:49:09 +0300

    adding styles for transaction history

--- src/components/TranscationHistory/TransactionHistory.jsx
+++ src/components/TranscationHistory/TransactionHistory.jsx
@@ -1,21 +1,23 @@
+import clsx from "clsx";
+import css from './TransactionHistory.module.css'
 export const TransactionHistory = ({items})=>{
 
 return(
 
-    <table>
-        <thead>
+    <table className={css.table}>
+        <thead className={css.head}>
         <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
+      <th className={css.title}>Type</th>
+      <th className={css.title}>Amount</th>
+      <th className={css.title}>Currency</th>
     </tr>
         </thead>
-        <tbody>
-        {items.map(item=>(
-            <tr key={item.id}>
-                <td>{item.type}</td>
-                <td>{item.amount}</td>
-                <td>{item.currency}</td>
+        <tbody className={css.body}>
+        {items.map((item, index)=>(
+            <tr className={clsx(css.row, index%2===0?[css.odd]:[css.even])  } key={item.id}>
+                <td className={css.data}>{item.type}</td>
+                <td className={css.data}>{item.amount}</td>
+                <td className={css.data}>{item.currency}</td>
             </tr>
 
         ))}
