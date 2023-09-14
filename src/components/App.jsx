b7d00033e01d1c15bf0ff665b8c0a9bc7413924e
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-09-14 18:47:01 +0300

    module styles for profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,10 @@ import data from '../data/data.json'
 import friends from '../data/friends.json'
 import transactions from '../data/transactions.json'
 
-import { Profile } from './Profile';
-import { Statistics } from './Statistics';
-import { FriendList } from './FriendList';
-import { TransactionHistory } from './TransactionHistory';
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TranscationHistory/TransactionHistory';
 
 
 export const App = () => {
