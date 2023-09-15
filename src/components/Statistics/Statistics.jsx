47db0dade22ed96f9f53389416cf760bef48588a
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-09-15 20:58:08 +0300

    fixing some of the styles

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,4 +1,4 @@
-
+import clsx from "clsx";
 import css from './Statistics.module.css'
 
 export const Statistics = props=> {
@@ -9,9 +9,9 @@ export const Statistics = props=> {
   <h2 className={css.title}>{title}</h2>
 
   <ul className={css.list}>
-    {stats.map(stat=>(
+    {stats.map((stat, index)=>(
       
-            <li className={css.item} key={stat.id}>
+            <li className={clsx(css.item, index%2===0? [css.odd]:[css.even])} key={stat.id}>
                 <span className={css.label}>{stat.label}</span>
                 <span className={css.percentage}>{stat.percentage}%</span>
             </li>
